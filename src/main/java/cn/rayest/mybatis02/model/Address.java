f092097c9b671cf845c76ab4cfa2043d2c400e27
Rayest <yi@rayest.cn>
2016-08-08 00:49:17 +0800

    modify one-one relation codes

---
+++ src/main/java/cn/rayest/mybatis02/model/Address.java
@@ -0,0 +1,53 @@
+package cn.rayest.mybatis02.model;
+
+/**
+ * Created by Rayest on 2016/6/24 0024.
+ */
+public class Address {
+    private Integer id;
+    private String province;
+    private String city;
+    private String district;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", province='" + province + '\'' +
+                ", city='" + city + '\'' +
+                ", district='" + district + '\'' +
+                '}';
+    }
+}
